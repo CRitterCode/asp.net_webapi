380503f70f58054d215e0ce12d8da75dc5f210fa
Ritter Christian <christian.ritter@edu.fh-joanneum.at>
2025-03-28 11:12:29 +0100

    boilerplate code for businesslogic

---
+++ Application/Guard/GuardService.cs
@@ -0,0 +1,37 @@
+﻿using Application.Exceptions;
+using Domain;
+using Domain.Interfaces;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Guard
+{
+    public static class GuardService
+    {
+        public static T NotNull<T>(T value, string message) where T : class
+        {
+            return value ?? throw new ServiceException(message);
+        }
+
+        public static async Task<T> NotNullAsync<T>(Task<T> task, string message) where T : class
+        {
+            var value = await task;
+            return NotNull(value, message);
+        }
+
+        public static async Task<T> CheckIfGuidIsValid<T>(IRepositoryGuidBase<T> repo, Guid guid) where T : class, IEntityGuidBase
+        {
+            return await NotNullAsync(repo.GetByGuidAsync(guid), "Couldn't find guid");
+        }
+
+        public static async Task<T> CheckIfIdIsValid<T>(IRepositoryBase<T> repo, uint id) where T : class, IEntityBase
+        {
+            return await NotNullAsync(repo.GetByIdAsync(id), "Couldn't find id");
+        }
+
+    }
+}
--- Application/Services/BusinessService.cs
+++ Application/Services/BusinessService.cs
@@ -1,19 +1,10 @@
 ﻿using Application.Exceptions;
+using Application.Guard;
 using Application.Interfaces;
 using Domain.Entities;
 using Domain.Entities.Reservation;
-using Domain.Enums;
-using Infrastructure.Exceptions;
 using Infrastructure.Interfaces;
-using Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Application.Services
 {
@@ -21,13 +12,12 @@ namespace Application.Services
     {
         public async Task AddTimeTableAsync(ICollection<TimeTable> timeTables)
         {
-            //var businessGuid = timeTables.First().Business.Guid;
-            //var business = await _businessRepository.GetByGuidAsync(businessGuid) ?? throw new ServiceException("Error with referenced Business.");
-
-            //if (!timeTables.All(tt => tt.BusinessId == business.Id))
-            //{
-            //    throw new ServiceException("Guid missmatch");
-            //}
+            var business = await GuardService.CheckIfGuidIsValid(_businessRepository, timeTables.First().Business.Guid);
+   
+            if (!timeTables.All(tt => tt.BusinessId == business.Id))
+            {
+                throw new ServiceException("Guid missmatch");
+            }
 
             var timeTablesgroupedAndSorted = timeTables
                                     .GroupBy(g => g.DayOfWeek)
@@ -42,14 +32,14 @@ namespace Application.Services
             {
                 if (!tt.TimeTable.All(t => t.IsValidWeekday))
                 {
-                    throw new ServiceException("Wrong Weekday provided.");
+                    throw new ServiceException("Wrong weekday provided.");
                 }
 
                 if (!tt.TimeTable.All(t => t.IsValidDayTimeCycle))
                 {
                     throw new ServiceException($"Wrong daycycle provided for {tt.DayOfWeek}");
                 }
-                var x = tt.TimeTable.Zip(tt.TimeTable.Skip(1), (current, next) => new { Current = current, Next = next });
+
                 if (tt.TimeTable.Zip(tt.TimeTable.Skip(1), (current, next) => new { Current = current, Next = next })
                                     .Any(pair => pair.Current.ClosingTime > pair.Next.OpeningTime))
                 {
@@ -60,6 +50,7 @@ namespace Application.Services
 
         }
 
+ 
 
     }
 }
